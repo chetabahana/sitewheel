5953f4edfcfaceb16b76e46a9e092af7d62ca72e
Chetabahana <chetabahana@gmail.com>
2019-11-28 07:06:34 +0700

    Update diagram.js

--- js/diagram.js
+++ js/diagram.js
@@ -95,7 +95,7 @@ function doTheTreeViz(diagram) {
                 // this is a hack so that click doesnt fire on the1st click of a dblclick
                 if (!diagram.nodeClickInProgress ) {
                     diagram.nodeClickInProgress = true;
-                    setTimeout(function(this){
+                    setTimeout(function(d,this){
                         if (diagram.nodeClickInProgress) {
                             diagram.nodeClickInProgress = false; draw.click(this);
                             //if (diagram.options.nodeFocus) {
