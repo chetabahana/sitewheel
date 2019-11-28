74595b71c1a9c76684350e3e0c2eddf4de49da39
Chetabahana <chetabahana@gmail.com>
2019-11-28 14:28:15 +0700

    Update diagram.js

--- js/diagram.js
+++ js/diagram.js
@@ -87,6 +87,7 @@ function doTheTreeViz(diagram) {
             .style("cursor", "pointer")
             //.attr("id", function(d,i) {return getId(d,i,this);})
             .attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")";})
+            .on("dblclick", function(d){draw.dblclick(d);})
         .call(force.drag);
 
     node.select("circle")
